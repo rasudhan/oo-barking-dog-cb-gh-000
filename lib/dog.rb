09d2a07cdd1c02b90ee4203ca177ee8baa2ece53
Anandha Sudhan <rasudhan@gmail.com>
2018-10-03 19:44:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+class Dog
+
+  def initialize(name,breed="Mutt")
+    @name=name
+    @breed=breed
+  end
+
+  def bark
+    puts "Woof!"
+  end
+
+  def name=(name)
+    @name=name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed=breed
+  end
+
+  def breed
+    @breed
+  end
+end
